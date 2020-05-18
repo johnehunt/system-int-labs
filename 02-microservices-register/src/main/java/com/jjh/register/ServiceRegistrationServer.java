d7df418cbe8e02fbb0ae10510c6d598d77de2aa5
John Hunt <john.hunt10@gmail.com>
2020-05-18 14:30:40 +0100

    SImplified configuration of register

--- 02-microservices-register/src/main/java/com/jjh/register/ServiceRegistrationServer.java
+++ 02-microservices-register/src/main/java/com/jjh/register/ServiceRegistrationServer.java
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @EnableEurekaServer
-@Configuration
 public class ServiceRegistrationServer {
 
     public static void main(String[] args) {
