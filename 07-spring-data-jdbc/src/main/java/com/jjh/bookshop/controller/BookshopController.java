335dbccf07b9b67b1f8e5f441a1ce1331328fe5c
John Hunt <john.hunt10@gmail.com>
2022-10-13 17:08:11 +0100

    Chnged url

--- 07-spring-data-jdbc/src/main/java/com/jjh/bookshop/controller/BookshopController.java
+++ 07-spring-data-jdbc/src/main/java/com/jjh/bookshop/controller/BookshopController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/books")
+@RequestMapping("books")
 public class BookshopController {
 
     private BookService service;
